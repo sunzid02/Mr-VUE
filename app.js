9f1ebfe6e683737a994ff70804d01e9f1a8af4fa
sunzid02 <sunzid02@gmail.com>
2019-03-19 23:08:08 +0600

    data and methods

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 new Vue({
+    //elements
     el:"#vue-app",
+    
     data:{
         name: 'zia',
+        job: 'Junior Developer',
+    },
+
+    //all functions
+    methods:{
+      greet: function(time) {
+          name = this.name; //getting name from data.name
+          return 'Good ' + time + ' '+ name;
+      },
+      
+      addition: function( num1, num2 ) {
+          return num1 + num2;
+      }
     }
 })
