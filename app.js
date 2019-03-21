f31b9bf2d3271e35be15851337a0733dd31e7397
sunzid02 <sunzid02@gmail.com>
2019-03-21 15:32:47 +0600

    coputed properties

--- app.js
+++ app.js
@@ -8,14 +8,29 @@ new Vue({
         gitlink: 'https://github.com/sunzid02/',
         web: '<a href = "https://github.com/sunzid02/">PUTA</a>',
         age: 24,
-        x:0,
-        y:0,
+        x: 0,
+        y: 0,
         write:'',
+        a: 0,
+        b: 0,
+        fruit: 20,
     },
 
     //all functions
     methods:{
+    //   addToA: function() {
+    //     console.log('addToA has been called');
+    //     return this.a + this.fruit;  
+    //   },
+
+    //   addToB: function() {
+    //     console.log('addToB has been called');
+    //     return this.b + this.fruit;  
+    //   },
+
       greet: function(time) {
+        //   console.log('greet has been called');
+
           name = this.name; //getting name from data.name
           return 'Good ' + time + ' '+ name;
       },
@@ -46,5 +61,17 @@ new Vue({
           console.log("U typed your name");
       }
 
+    },
+
+    computed:{
+        addToA: function () {
+            console.log('addToA has been called');
+            return this.a + this.fruit;
+        },
+
+        addToB: function () {
+            console.log('addToB has been called');
+            return this.b + this.fruit;
+        },
     }
 })
