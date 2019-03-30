3c3c562ca88432e1362c41efe97d36bda0069882
sunzid02 <sunzid02@gmail.com>
2019-03-30 17:05:18 +0600

    Nesting Components

---
+++ myvuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h2>Hello ninja component</h2>
+        <ul>
+            <li v-for="ninja in ninjas"> {{ ninja }} </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      ninjas: ['Zia', 'Zayed', 'Zunayed']
+    }
+  },
+}
+</script>
