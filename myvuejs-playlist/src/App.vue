824af72a5a5e892d27179b43fdcef9515a5d1c89
sunzid02 <sunzid02@gmail.com>
2019-04-01 18:33:02 +0600

    first com

--- myvuejs-playlist/src/App.vue
+++ myvuejs-playlist/src/App.vue
@@ -1,28 +1,16 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-
-    <ninjas></ninjas>
-
-  </div>
+  <Header></Header>
 </template>
 
 <script>
-// import Ninjas from './Ninjas.vue'
 export default {
-  // components:{
-  //   'ninjas': Ninjas
-  // },
   data () {
     return {
-      title: 'Ninja APP'
+
     }
   },
 }
 </script>
 
-<style scoped>
-  h1 {
-    color:blue
-  }
+<style>
 </style>
