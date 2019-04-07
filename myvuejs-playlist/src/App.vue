fc2e8e5a24e24782381bebf33949d37cc01b0bdc
sunzid02 <sunzid02@gmail.com>
2019-04-07 18:10:38 +0600

    basic of slot

--- myvuejs-playlist/src/App.vue
+++ myvuejs-playlist/src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div>
-
+    <form-helper>
+      <h2 slot="title"> {{ title }} </h2>
+      <p slot="text">I am the paragraph text</p>
+    </form-helper>
   </div>
 </template>
 
 <script>
+  import formHelper from './components/formHelper.vue'
 
+  export default {
+    components:{
+      'form-helper': formHelper
+    },
+    data () {
+      return {
+        title:'I am the dynamic slot title',
+      }
+    },
 
+    methods:{
 
-export default {
-  components:{
-
-  },
-  data () {
-    return {
-
-    }
-  },
-
-  methods:{
-
-  },
-}
+    },
+  }
 </script>
 
-<style>
+<style >
 </style>
---
+++ myvuejs-playlist/src/components/formHelper.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <slot name="title"></slot>      
+      <h1>I am the form helper</h1>
+      <slot name="text"></slot>
+  </div>
+</template>
+ 
+<script>
+
+    export default {
+        components:{
+
+        },
+        data () {
+            return {
+                // title:'I am the dynamic slot title',
+            }
+        },
+
+        methods:{
+
+        },
+    }
+</script>
+
+<style scoped>
+    h1{
+        color: red;
+    }
+</style>
