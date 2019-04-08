d8e46dff52f958fc3b4b309446f8f557be8d6cd6
sunzid02 <sunzid02@gmail.com>
2019-04-08 14:42:11 +0600

    post request added with vue-resource

--- myvuejs-playlist/src/main.js
+++ myvuejs-playlist/src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 // import Header from './components/Header.vue'
 // Vue.component('Header', Header)
 
+import VueResource from 'vue-resource'
+Vue.use(VueResource);
+
 // creating event bus
 // export const bus = new Vue();
 
