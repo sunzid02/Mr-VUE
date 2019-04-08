cdc923c246bc58b4d4f596fcede52edf4a970928
sunzid02 <sunzid02@gmail.com>
2019-04-08 17:57:58 +0600

    Get req

--- myvuejs-playlist/src/App.vue
+++ myvuejs-playlist/src/App.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
-      <add-blog></add-blog>
+      <show-blogs></show-blogs>
     </div>
 </template>
 
 <script>
 // Imports
 import addBlog from './components/addBlog.vue';    // './' means current directory
+import showBlogs from './components/showBlogs.vue';   
 
 export default {
     components: {
       'add-blog': addBlog,
+      'show-blogs': showBlogs,
     },
     data () {
         return {
