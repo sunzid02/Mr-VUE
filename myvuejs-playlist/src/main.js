c4b5a9bd09685485dc0744d58f01f5b8cd106828
sunzid02 <sunzid02@gmail.com>
2019-04-10 19:45:42 +0600

    custom directives added

--- myvuejs-playlist/src/main.js
+++ myvuejs-playlist/src/main.js
@@ -10,6 +10,34 @@ Vue.use(VueResource);
 // creating event bus
 // export const bus = new Vue();
 
+//creating globally custom directives
+Vue.directive('rainbow', {
+  bind(el, binding, vnode){
+    el.style.color = "#" + Math.random().toString().slice(2, 8);
+  }
+});
+
+Vue.directive('theme', {
+  bind(el, binding, vnode){
+    
+      if (binding.value == 'wide') 
+      {
+         el.style.maxWidth = '1200px'; 
+      }
+      else if(binding.value == 'narrow')
+      {
+        el.style.maxWidth = '580px'; 
+      }
+
+      if (binding.arg == 'column') 
+      {
+        el.style.background = '#ddd';
+        el.style.padding = '20px';
+      }
+
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
