3d1469277aaed5e9c6299257f28a36082c8137e0
sunzid02 <sunzid02@gmail.com>
2019-04-10 20:09:24 +0600

    custom filters  added

--- myvuejs-playlist/src/main.js
+++ myvuejs-playlist/src/main.js
@@ -16,27 +16,35 @@ Vue.directive('rainbow', {
     el.style.color = "#" + Math.random().toString().slice(2, 8);
   }
 });
-
 Vue.directive('theme', {
-  bind(el, binding, vnode){
-    
-      if (binding.value == 'wide') 
-      {
-         el.style.maxWidth = '1200px'; 
-      }
-      else if(binding.value == 'narrow')
-      {
-        el.style.maxWidth = '580px'; 
-      }
+  bind(el, binding, vnode) {
 
-      if (binding.arg == 'column') 
-      {
-        el.style.background = '#ddd';
-        el.style.padding = '20px';
-      }
+    if (binding.value == 'wide') {
+      el.style.maxWidth = '1200px';
+    }
+    else if (binding.value == 'narrow') {
+      el.style.maxWidth = '580px';
+    }
+
+    if (binding.arg == 'column') {
+      el.style.background = '#ddd';
+      el.style.padding = '20px';
+    }
 
   }
 });
+
+
+//Custom Filter
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value){
+  return value.slice(0,100) + '...';
+});
+
+
 
 new Vue({
   el: '#app',
